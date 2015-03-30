a3454f923ec8c20dbfee8b97ffe51213e31afa31
Brian <bzh8205@rit.edu>
2015-03-30 14:14:03 -0400

    Added 10hz timer

--- RealTimeControlProject/ThreadRunner.c
+++ RealTimeControlProject/ThreadRunner.c
@@ -8,9 +8,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/neutrino.h> //channel creatte
+#include <sys/neutrino.h> //channel creatte, pulses too
 #include <pthread.h>    // pthread_create/exit
 #include <sys/netmgr.h> // channel attach
+#include <assert.h> //for timer create
 #include "ThreadRunner.h"
 #include "AnalogInputThread.h"
 #include "AnalogOutputThread.h"
@@ -31,8 +32,51 @@ static int CHANNEL_IDS[4];
 static int CONNECTION_IDS[4];
 static int NUM_THREADS = 4;
 
+//Creates channel and sends pulse with pulseid on it every nsfreq.
+int InitializeTimer(long nsfreq, int pulseid){
+  int chid;
+  timer_t timer_id;
+  struct sigevent event;
+  struct itimerspec timer;
+  printf("Initializing Pulses at %ldns with pulse id as %d\n", nsfreq, pulseid);
+
+  /* Create a channel to receive timer events on. */
+  chid = ChannelCreate( 0 ); //b: opening up a channel to send messages through
+  assert ( chid != -1 );      // if returns a -1 for failure we stop with error
+  /* Set up the timer and timer event. */
+  event.sigev_notify = SIGEV_PULSE;   // most basic message we can send -- just a pulse number
+  event.sigev_coid = ConnectAttach ( ND_LOCAL_NODE, 0, chid, 0, 0 );  // Get ID that allows me to communicate on the channel
+  assert ( event.sigev_coid != -1 );    // stop with error if cannot attach to channel
+  event.sigev_priority = getprio(0);
+  event.sigev_value.sival_int = pulseid;
+
+  // Now create the timer and get back the timer_id value for the timer we created.
+  if ( timer_create( CLOCK_REALTIME, &event, &timer_id ) == -1 )  // CLOCK_REALTIME available in all POSIX systems
+  {
+    perror ( "cannot create timer\n" );
+    exit( EXIT_FAILURE );
+  }
+
+  /* Change the timer request to alter the behavior. */
+  timer.it_value.tv_sec = 0;
+  timer.it_value.tv_nsec = nsfreq;    // interrupt at .1 ms.
+  timer.it_interval.tv_sec = 0;
+  timer.it_interval.tv_nsec = nsfreq; // keep interrupting every .1 ms.
+
+  /* Start the timer. */
+  if ( timer_settime( timer_id, 0, &timer, NULL ) == -1 )
+  {
+    perror("cannot start timer.\n");
+    exit( EXIT_FAILURE );
+  }
+
+  return chid;
+}
+
 void startThreads(){
+  struct _pulse pulse; //for timer
   int iter;
+  int chid, pid; //for timer
   //threads need channels to receive messages
   printf("Creating channels\n");
   for( iter = 0 ; iter < NUM_THREADS ; iter++ ){
@@ -57,12 +101,14 @@ void startThreads(){
   if( USER_IN_RUN_THREADS == 1 ) {
 
     printf("Thread Runner: starting PID loop.\n");
+    chid = InitializeTimer(1000000, 1); //set timer to 10hz or 100ms
 
     //Tests messaging chain
     ThreadMessage msg = { 0 , 0 };
     int reply_status;
 
     while( USER_IN_RUN_THREADS ){
+      pid = MsgReceivePulse ( chid, &pulse, sizeof( pulse ), NULL );
       reply_status = MsgSend(CONNECTION_IDS[A_IN_THREAD_ID], &msg, sizeof(msg), &msg, sizeof(msg));
       printf("Analog Input result: %f\n", msg.value);
       reply_status = MsgSend(CONNECTION_IDS[CONTROL_THREAD_ID], &msg, sizeof(msg), &msg, sizeof(msg));
@@ -93,3 +139,5 @@ void endThreads(){
     ChannelDestroy(CHANNEL_IDS[iter]);
   }
 }
+
+
